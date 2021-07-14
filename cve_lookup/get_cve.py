1204fcf3e0684b40c1d24d3b7d15e75886cec46c
Mason Fisher <sackzombie10@gmail.com>
2021-07-14 19:04:13 -0400

    Made a function that gets data

--- cve_lookup/get_cve.py
+++ cve_lookup/get_cve.py
@@ -4,4 +4,12 @@ import requests
 nvd = requests.get("https://nvd.nist.gov/vuln/detail/CVE-2014-0160")
 
 nvd_html = BeautifulSoup(nvd.text, 'html.parser')
-print(nvd_html.find(attrs={"data-testid":"page-header-vuln-id"}).string)
+
+def get_id(id, testid=True):
+    if testid:
+        return nvd_html.find(attrs={"data-testid":id}).string
+    else:
+        return nvd_html.find(attrs={"id":id}).string
+print(get_id("page-header-vuln-id"))
+print(get_id("Cvss3NistCalculatorAnchor", testid=False))
+print(get_id("Cvss2CalculatorAnchor", testid=False))
