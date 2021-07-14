21f2be9ff4f08f1186f74763e1e3017997a8fd0f
Mason Fisher <sackzombie10@gmail.com>
2021-07-14 19:26:10 -0400

    Made cve class

--- cve_lookup/__main__.py
+++ cve_lookup/__main__.py
@@ -1,1 +1,6 @@
 from . import get_cve
+
+heartbleed = get_cve.cve("CVE-2014-0160")
+print(heartbleed.id)
+print(heartbleed.cvss3)
+print(heartbleed.cvss2)
--- cve_lookup/get_cve.py
+++ cve_lookup/get_cve.py
@@ -1,15 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-nvd = requests.get("https://nvd.nist.gov/vuln/detail/CVE-2014-0160")
+class cve():
+    id = None
+    cvss3 = None
+    cvss2 = None
 
-nvd_html = BeautifulSoup(nvd.text, 'html.parser')
+    def __init__(self, id):
+        try:
+            assert(type(id) == str)
+            assert(id[:4].upper() == 'CVE-')
+        except AssertionError:
+            raise ValueError('{} is not a valid CVE id!'.format(id))
+        nvd = requests.get("https://nvd.nist.gov/vuln/detail/{}".format(id.upper()))
+        nvd_html = BeautifulSoup(nvd.text, 'html.parser')
 
-def get_id(id, testid=True):
-    if testid:
-        return nvd_html.find(attrs={"data-testid":id}).string
-    else:
-        return nvd_html.find(attrs={"id":id}).string
-print(get_id("page-header-vuln-id"))
-print(get_id("Cvss3NistCalculatorAnchor", testid=False))
-print(get_id("Cvss2CalculatorAnchor", testid=False))
+        def _get_id(id, testid=True):
+            if testid:
+                return nvd_html.find(attrs={"data-testid":id}).string
+            else:
+                return nvd_html.find(attrs={"id":id}).string
+
+        self.id = _get_id("page-header-vuln-id")
+        self.cvss3 = _get_id("Cvss3NistCalculatorAnchor", testid=False)
+        self.cvss2 = _get_id("Cvss2CalculatorAnchor", testid=False)
