286214eba5d533e9d2a268a5f5f4d3ee60218392
Mason Fisher <sackzombie10@gmail.com>
2021-08-01 13:30:12 -0400

    Tests now use the random vectors but doesn't work because cvsslib isn't calculating correctly?

--- tests/cvss2_test.py
+++ tests/cvss2_test.py
@@ -1,5 +1,4 @@
 from cve_lookup.vector import cvss2_vector
-import unittest
 import os.path
 
 class TestScore(unittest.TestCase):
@@ -8,40 +7,69 @@ class TestScore(unittest.TestCase):
         tests = open(self.file, 'r')
         line = 0
         vector = None
-        base_score = None
-        temp_score = None
-        envi_score = None
+        score = None
         calc = True
         for i in tests:
             if line == 0:
                 vector = i[:-1]
             elif line == 1:
-                base_score = i[:-1]
-                if base_score == 'None':
+                calc = True
+                score = i[:-1]
+                if score == 'None':
                     calc = False
                 else:
-                    base_score = float(base_score)
-            elif line == 2:
-                temp_score = i[:-1]
-                if temp_score == 'None':
-                    calc = False
-                else:
-                    temp_score = float(temp_score)
+                    score = float(score)
             elif line == 3:
-                envi_score = i[:-1]
-                if envi_score == 'None':
-                    calc = False
-                else:
-                    envi_score = float(envi_score)
                 line = -1
                 if calc:
-                    self.assertEqual(round(cvss2_vector(str(vector)).score_base, 1), base_score)
-                    self.assertEqual(round(cvss2_vector(str(vector)).score_temporal, 1), temp_score)
-                    self.assertEqual(round(cvss2_vector(str(vector)).score_environmental, 1), envi_score)
+                    self.assertEqual(round(cvss2_vector(str(vector)).score_base, 1), score)
             line += 1
         tests.close()
 
+    def test_temporal(self):
+        tests = open(self.file, 'r')
+        line = 0
+        vector = None
+        score = None
+        calc = True
+        for i in tests:
+            if line == 0:
+                vector = i[:-1]
+            elif line == 2:
+                calc = True
+                score = i[:-1]
+                if score == 'None':
+                    calc = False
+                else:
+                    score = float(score)
+            elif line == 3:
+                line = -1
+                if calc:
+                    self.assertEqual(round(cvss2_vector(str(vector)).score_temporal, 1), score)
+            line += 1
+        tests.close()
 
+    def test_environmental(self):
+        tests = open(self.file, 'r')
+        line = 0
+        vector = None
+        score = None
+        calc = True
+        for i in tests:
+            if line == 0:
+                vector = i[:-1]
+            elif line == 3:
+                calc = True
+                score = i[:-1]
+                if score == 'None':
+                    calc = False
+                else:
+                    score = float(score)
+                line = -1
+                if calc:
+                    self.assertEqual(round(cvss2_vector(str(vector)).score_environmental, 1), score)
+            line += 1
+        tests.close()
 
 if __name__ == '__main__':
     unittest.main()
